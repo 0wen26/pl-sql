52ae842fe534216e4d2c36e788af5b3d8a899a2a
0wen26 <raydroid26@gmail.com>
2021-03-05 16:28:25 +0100

    ejer2

--- ejer2.sql
+++ ejer2.sql
@@ -1,3 +1,4 @@
+    set SERVEROUTPUT ON;
 declare
     --Declaro cursor para recuperar department_id  menores de 100
     CURSOR c_dept_cursor IS
@@ -12,12 +13,17 @@ declare
       from employees
      where department_id = deptno  and employee_id < 120;
      --Variables
-     v_emp employees.department_id%type;
-     v_dept departments.department_id%type;
+     v_emp departments.department_id%type;
+     v_dept departments.department_name%type;
 
 begin
     open c_dept_cursor;
         LOOP
+            fetch c_dept_cursor into v_emp,v_dept;
+            exit when c_dept_cursor%notfound;
+            dbms_output.put_line(v_emp);
+
+        end loop;
         
     close c_dept_cursor;
 
