7019f0a9b0c0b63ae437c325476b95a1fe2f2ce4
0wen26 <raydroid26@gmail.com>
2021-03-05 15:24:27 +0100

    ejer2

---
+++ ejer2.sql
@@ -0,0 +1,33 @@
+declare
+    --Declaro cursor para recuperar department_id  menores de 100
+    CURSOR c_dept_cursor IS
+    select department_id, department_name
+      from departments
+     where department_id < 100
+     order by department_id;
+    --Declaro cursor al que paso un parametro y compruevo que sea menor de 120 el employee id
+    -- del departamento que paso como parametro
+    CURSOR c_emp_cursor  (deptno number) IS
+    select last_name,job_id,hire_date, salary
+      from employees
+     where department_id = deptno  and employee_id < 120;
+     --Variables
+     v_emp c_emp_cursor%rowtype;
+     v_dept c_dept_cursor%rowtype;
+
+begin
+    open c_dept_cursor;
+        LOOP
+        
+    close c_dept_cursor;
+
+
+
+    open c_emp_cursor(10);
+
+    close c_emp_cursor;
+
+
+
+
+end;
