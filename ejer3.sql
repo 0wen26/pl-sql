10ea96fe47f203e7bc9462c0e08ece91e481a424
0wen26 <raydroid26@gmail.com>
2021-03-07 12:59:44 +0100

    ejer3 fin

--- ejer3.sql
+++ ejer3.sql
@@ -1,7 +1,7 @@
-/*
+
 CREATE TABLE top_salaries(
     salary NUMBER(7,2));
-*/
+
 desc EMPLOYEES;
 declare
 
@@ -12,15 +12,24 @@ declare
       SELECT SALARY
       FROM EMPLOYEES
       order by salary desc;
+   v_emp_record c_emp_cursor%rowtype;
+   v_count number:=0;
 BEGIN
    open c_emp_cursor;
       LOOP
+         fetch c_emp_cursor INTO v_emp_record;
          
-
-
+         exit when v_count=v_num;
+            insert into top_salaries (salary)
+            values (v_emp_record.SALARY);
+            v_count:=v_count+1;
       END LOOP;
 
 
 
 END;
 /
+--drop table  top_salaries;
+
+
+select * from top_salaries;
